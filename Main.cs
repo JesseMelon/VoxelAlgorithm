40000f74a91ba308e54f14aa15ec5f48c40fcd5a
JesseMelon <Jessechambers2000@icloud.com>
2024-02-20 14:19:09 -0500

    Implimented blender imports and meshs folder

--- Main.cs
+++ Main.cs
@@ -6,9 +6,21 @@ public partial class Main : Node3D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		MeshInstance3D cubeMeshInstance3D = GetNode<MeshInstance3D>("Cube");
 
-		Mesh cubeMesh = cubeMeshInstance3D.Mesh;
+		Node3D basicCubeNode = GetNode<Node3D>("BasicCube");
+		MeshInstance3D cubeMeshInstance3D = GetNode<MeshInstance3D>("BasicCube / Cube");
+		if (basicCubeNode != null)
+			GD.Print("Got blend node");
+		if (cubeMeshInstance3D != null)
+			GD.Print("Got mesh node");
+
+
+
+		//Mesh cubeMesh = cubeMeshInstance3D.Mesh;
+
+		MeshDataTool mdt = new();
+
+		//mdt.CreateFromSurface((ArrayMesh)cubeMesh, 0);
 
 	}
 
